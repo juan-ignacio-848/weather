92527479673c0cf59a25623089379b3d3ee66746
nmkip <juan.ignacio.lopez@redb.ee>
2019-08-19 00:38:42 -0300

    Renamed class

--- src/main/java/com/nmkip/weather/WeatherApplication.java
+++ src/main/java/com/nmkip/weather/WeatherApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClimaApplication {
+public class WeatherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClimaApplication.class, args);
+        SpringApplication.run(WeatherApplication.class, args);
     }
 
 }
